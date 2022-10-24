abb1c14bd712012b7da77670ac170d57a44685b5
amit kumar <104476740+itsamitstyle@users.noreply.github.com>
2022-10-24 08:14:39 +0530

    calculate the area of rectangle

---
+++ calculate the area of rectagle.cpp
@@ -0,0 +1,17 @@
+//calculate the arae of rectangle input from the user 
+#include <iostream>
+using namespace std ;
+int main (){
+    int leangth ;
+    int breadth ;
+    cout <<"enter the value of leangth";
+    scanf("%d" , &leangth);
+    cout <<"enter the value of breadth ";
+    scanf("%d" , &breadth);
+    int area ;
+    area = leangth*breadth;
+    cout <<"%d\n "<<area ;
+    
+    return 0;
+
+}
