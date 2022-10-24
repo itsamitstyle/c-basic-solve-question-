9e9d73214a3e01c2eedc2f53328ed7117d3f6905
amit kumar <104476740+itsamitstyle@users.noreply.github.com>
2022-10-24 08:10:28 +0530

    convert celcious to fernhite

---
+++ convert celcious to fernhite.cpp
@@ -0,0 +1,14 @@
+//convert the celcious to fernhite 
+#include <iostream>
+using namespace std;
+int main (){
+    int celcious ;
+    cout <<"enter the value of celcious ";
+    scanf("%d" , &celcious);
+    int fernhite ;
+    fernhite  = celcious*9/5+32;
+    cout <<"the value of fernhite is %d\n "<<fernhite;
+    
+    return 0;
+
+}
