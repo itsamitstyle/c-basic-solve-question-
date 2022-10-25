efe1bdae846c655518a135f6c1c0ff6568830738
amit kumar <104476740+itsamitstyle@users.noreply.github.com>
2022-10-25 18:37:10 +0530

    CRAEATE A LETTER USING #

---
+++ create a h using #.cpp
@@ -0,0 +1,32 @@
+//making a hc using # 
+
+#include <iostream>
+using namespace std;
+int main (){
+    //create a h using # 
+    int a=1;
+    int b =0;
+    if (a>b)
+    {
+        /* code */
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#############\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    cout <<"#           #\n";
+    }
+    else{
+        cout <<"false ";
+
+    }
+    
+
+    
+    return 0;
+
+}
