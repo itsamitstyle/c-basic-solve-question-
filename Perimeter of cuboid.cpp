b1e2435687e8b38dde4b57501969409c727532df
amit kumar <104476740+itsamitstyle@users.noreply.github.com>
2022-10-26 16:29:40 +0530

    Perimeter of cuboid

---
+++ Perimeter of cuboid.cpp
@@ -0,0 +1,21 @@
+//PERIMERTER OF CUBIDE 
+
+#include<iostream>
+using namespace std;
+int main(){
+    //perimeter of cubide 
+    int leanght;
+    int breadth;
+    int height;
+    cout <<"enter the value of leanght ";
+    scanf("%d",&leanght);
+    cout <<"enter the value of breadth ";
+    scanf("%d",&breadth);
+    cout <<"enter the value of height ";
+    scanf("%d",&height);
+    int cubide ;
+    cubide = 4*leanght*breadth*height;
+    cout <<"the value of cubide is %d\n "<<cubide;
+
+    return 0;
+}
